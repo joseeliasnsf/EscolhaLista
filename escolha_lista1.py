6a6068e9dba3e71035b5e4ee136a0b5538b4478a
Jose Elias Nunes Silva Filho <jensf_dev@proton.me>
2023-08-26 23:12:13 -0300

    Lista
    Mudança

--- escolha_lista1.py
+++ escolha_lista1.py
@@ -4,5 +4,5 @@ p2 = input('Digite o nome da pessoa 2: ')
 p3 = input('Digite o nome da pessoa 3: ')
 p4 = input('Digite o nome da pessoa 4: ')
 lista = [p1, p2, p3,p4]
-sorteio = random.choice(lista)
-print('A pessoa sorteada foi: ', sorteio)
+sorteio = random.shuffle(lista)
+print(lista)
